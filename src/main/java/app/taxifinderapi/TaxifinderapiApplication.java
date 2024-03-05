f3dd3dc30d84fb822fda18e833fe3cd7951beafc
Mduduzi-Jele <jelemduduzisa@gmail.com>
2024-03-05 09:51:12 +0200

    init

---
+++ src/main/java/app/taxifinderapi/TaxifinderapiApplication.java
@@ -0,0 +1,13 @@
+package app.taxifinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxifinderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxifinderapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/taxifinderapi/TaxifinderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.taxifinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxifinderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
